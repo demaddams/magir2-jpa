bc0a901de50bb89dbcc6d97ff5d5ee00db998df1
Philippe Kouao <philippe.kouao@gmail.com>
2016-07-29 18:39:02 +0000

    initial version

---
+++ src/main/java/jpa/Main.java
@@ -0,0 +1,20 @@
+package jpa;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+	private static final Logger LOGGER = Logger.getLogger("JPA");
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		
+	}
+        
+        public void run() {
+            LOGGER.log(Level.INFO, "Starting..");
+            //code here
+            LOGGER.log(Level.INFO, "Done.");
+        }
+
+}
